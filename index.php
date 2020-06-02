b507c082b4e8f6ea7265d6ed1366094ec00d3f4c
IrynaAndr <63851046+IrynaAndr@users.noreply.github.com>
2020-06-02 13:49:25 +0300

    Adding comments
    Adding comments to main page

--- index.php
+++ index.php
@@ -90,7 +90,178 @@
     </div>
 
     <hr class="featurette-divider">
+ <div style="background: #cde4f7; padding-left: 40px ">
+        </br>
+    <h2  style="text-align:center; padding-right: 30%"> Patient's reviews :</h2>
+    <h4  style="text-align:center; padding-right: 30%"> Last 3:</h4>
+    <br>
+    
+    
+        <?php
+      $mysql = new mysqli('localhost','id13682932_hh','123123123-Asd','id13682932_hbd');
+      $sql = "SELECT * FROM Feedback ORDER BY Id_feedback DESC LIMIT 3;";
+      
+      
+      $result = $mysql->query($sql);
+      if ($result->num_rows > 0) {
+        // output data of each row
+        
+        
+        while($row = $result->fetch_assoc() ) {
+            $idpat= $row["Id_patient"];
+            $sql2 = "SELECT Name, Surname From Patient WHERE Id_patient='$idpat'";
+            $res2= $mysql->query($sql2);
+             while($row2 = $res2->fetch_assoc()) {
+                 
+                 if($row["Mark"]==5){
+                     
+                     echo '
+                     <div id="wrapper">
+  <div id="left" style="padding:20px; ">
+   <h4 style="color:#312a80; text-align:left; " >'; echo  $row2["Surname"]." ".  $row2["Name"] ; echo' :</h4>
+  <p id="rcorners1">'; echo $row["Feedback"]; echo'</p>
+  </div>
+  <div id="right" style="padding:20px; margin-top: -60px; margin-left:-500px ">
+    <p class="crop"><a href="#" title="">  <img src="https://www.harpendencleaning.co.uk/wp-content/uploads/2018/04/5-star-rating.png" style="margin:-200px -20px -200px -40px;" alt="" /></a></p></p>
+  </div>
+  
+</div>
+                     ';
+                 }
+                 else if($row["Mark"]==4){
+                     
+                      echo '
+                     <div id="wrapper">
+  <div id="left" style="padding:20px; ">
+   <h4 style="color:#312a80; text-align:left; " >'; echo  $row2["Surname"]." ".  $row2["Name"] ; echo' :</h4>
+  <p id="rcorners1">'; echo $row["Feedback"]; echo'</p>
+  </div>
+  <div id="right" style="padding:20px; margin-top: -60px; margin-left:-430px ">
+    <p class="crop"><a href="#" title="">  <img src="https://www.harpendencleaning.co.uk/wp-content/uploads/2018/04/5-star-rating.png" style="margin:-200px -190px -200px -40px;" alt="" /></a></p></p>
+  </div>
+  
+</div>
+                     ';   
+                 }
+                     else if($row["Mark"]==3){
+                        
+                      echo '
+                     <div id="wrapper">
+  <div id="left" style="padding:20px; ">
+   <h4 style="color:#312a80; text-align:left; " >'; echo  $row2["Surname"]." ".  $row2["Name"] ; echo' :</h4>
+  <p id="rcorners1">'; echo $row["Feedback"]; echo'</p>
+  </div>
+  <div id="right" style="padding:20px; margin-top: -60px; margin-left:-377px ">
+    <p class="crop"><a href="#" title="">  <img src="https://www.harpendencleaning.co.uk/wp-content/uploads/2018/04/5-star-rating.png" style="margin:-200px -333px -200px -40px;" alt="" /></a></p></p>
+  </div>
+  
+</div>
+                     ';   
+                 }
+                 else if($row["Mark"]==2){
+                     
+                      echo '
+                     <div id="wrapper">
+  <div id="left" style="padding:20px; ">
+   <h4 style="color:#312a80; text-align:left; " >'; echo  $row2["Surname"]." ".  $row2["Name"] ; echo' :</h4>
+  <p id="rcorners1">'; echo $row["Feedback"]; echo'</p>
+  </div>
+  <div id="right" style="padding:20px; margin-top: -60px; margin-left:-325px ">
+    <p class="crop"><a href="#" title="">  <img src="https://www.harpendencleaning.co.uk/wp-content/uploads/2018/04/5-star-rating.png" style="margin:-200px -475px -200px -40px;" alt="" /></a></p></p>
+  </div>
+  
+</div>
+                     ';   
+                 }
+                 else {
+                     
+              echo '
+                     <div id="wrapper">
+  <div id="left" style="padding:20px; ">
+   <h4 style="color:#312a80; text-align:left; " >'; echo  $row2["Surname"]." ".  $row2["Name"] ; echo' :</h4>
+  <p id="rcorners1">'; echo $row["Feedback"]; echo'</p>
+  </div>
+  <div id="right" style="padding:20px; margin-top: -60px; margin-left:-262px ">
+    <p class="crop"><a href="#" title="">  <img src="https://www.harpendencleaning.co.uk/wp-content/uploads/2018/04/5-star-rating.png" style="margin:-200px -620px -200px -40px;" alt="" /></a></p></p>
+  </div>
+  
+</div>
+                     ';  
+                 }
+        }}
+      }
+      
+      $mysql->close();
+      ?>
+    <p style="width=100%; text-align:center;"><a href="https://tridimensional-laun.000webhostapp.com/CommTest.php" class="button">See more</a> </p>
+    </br>
+    </div>
+    <div style="background: #71b9f5; margin:auto;padding:10px; padding-left:25%">
+      <?php
+      error_reporting(0);
+      if( $_COOKIE['userL'] != null || $_COOKIE['userL'] != "" ){
+        echo'
+      <div class="Add" style="width:80%; margin:auto;">
+     <h2 > Leave Feedback:</h2>
+     <form action="sc/submitcomm.php" method="post">
+       <p>Rating (from 1 to 5):</p>
+       <input type="range" id="Mark" name="Mark" min="0" max="5">
+       <p>Comment:</p>
+       <input type="text" name="Feedback" style="width: 500px;height:80px;">
+     </br>
+     </br>
+       <input type="submit" value="submit" >
+     </form>
 
+    </div>
+    ';
+    }
+    else{
+      echo'<div style="margin:auto; width: 100%; padding: 10px; padding-left: 25%">
+    <p><h5>Please log in to left a Feedback 
+    <a href="https://tridimensional-laun.000webhostapp.com/loginF.php" class="button">Log in</a></h5></p>
+      </div>
+      ';
+    }
+    ?>
+<style>
+  #rcorners1 {
+  border-radius: 15px;
+  border: 1px solid #312a80;
+  background: white;
+  padding: 5px; 
+   
+}
+#wrapper {
+  padding-left: 15%;
+  display: flex;
+}
+
+#left {
+  flex: 0 0 75%;
+}
+
+#right {
+  flex: 1;
+}
+.crop{
+	float:left;
+  	transform: scale(0.2, 0.2);
+	
+	overflow:hidden; /* this is important */
+	}
+	
+.crop img{
+        
+	}
+</style>
+    <style>
+    .button {
+     border: none !important; display:inline-block !important;text-align: center !important;padding: 7px 20px !important;
+        color: #fff !important; font-size:16px !important; font-weight: 600 !important; font-family:OpenSans, sans-serif; cursor: pointer !important; border-radius: 2px !important;
+        background: rgb(43,119,183) !important;"onmouseover="this.style.opacity='0.5';" onmouseout="this.style.opacity='1';"
+    }
+    </style>
 
   </body>
 
